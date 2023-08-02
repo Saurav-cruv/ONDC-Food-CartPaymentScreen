e937443980b0d86be6aad71dd86c80653d843899
Saurav-K-S <134520449+Saurav-cruv@users.noreply.github.com>
2023-08-02 20:11:35 +0530

    Basic column structure created

--- ondc_cartpaymentscreen/lib/main.dart
+++ ondc_cartpaymentscreen/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ondc_cartpaymentscreen/widgets/map_module.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,46 @@ class MyApp extends StatelessWidget {
                     stops: const [0.6, 1]),
               ),
             ),
-            
+            Column(
+              children: [
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          shape: CircleBorder(),
+                          backgroundColor: Colors.transparent),
+                      onPressed: () {},
+                      child: const Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        color: Colors.black,
+                        size: 12.5,
+                      )),
+                  Column(
+                    children: [
+                      Text('THE RAJPUT ROOM',
+                          style: GoogleFonts.jetBrainsMono(fontSize: 12)),
+                      const Text('Rambagh Palace',
+                          style: TextStyle(fontFamily: 'PPMori', fontSize: 10))
+                    ],
+                  )
+                ]),
+                const Row(
+                  children: [Text('item row')],
+                ),
+                const Row(
+                  children: [Text('delivery fee row')],
+                ),
+                const Row(
+                  children: [Text('tax row')],
+                ),
+                const Row(
+                  children: [Text('pay row')],
+                ),
+                const Text('delivery time'),
+                const Text('time'),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text('Proceed to pay'))
+              ],
+            )
           ],
         ),
       ),
